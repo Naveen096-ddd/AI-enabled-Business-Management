1a911ea0ef582f7407aa6fb57171155bcb2f374e
Naveen096-ddd <naveen.menda96@gmail.com>
2026-01-08 12:19:03 +0530

    AI-enabled2

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, lazy, Suspense } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import './index.css';
 import { ThemeProvider, CssBaseline, CircularProgress, Box } from '@mui/material';
 import theme from './components/footer/Theme.jsx';
