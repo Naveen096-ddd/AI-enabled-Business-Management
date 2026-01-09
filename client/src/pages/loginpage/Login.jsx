ca349d3c226558ec234160554a4a1b8e571aa514
Naveen096-ddd <naveen.menda96@gmail.com>
2026-01-09 16:57:51 +0530

    Ai-enable2

--- client/src/pages/loginpage/Login.jsx
+++ client/src/pages/loginpage/Login.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import "./login.css";
 import { useDispatch, useSelector } from "react-redux";
-import { loginUser } from "../../app/AuthSlice";
+import { loginUsers } from "../../app/AuthSlice";
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
