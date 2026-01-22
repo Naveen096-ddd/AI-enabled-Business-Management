71969ff66af9a3190e0ce2c9adf4921696dd6d63
Naveen096-ddd <naveen.menda96@gmail.com>
2026-01-22 10:04:41 +0530

    'AI-enabled'

--- Hackathon/client/src/App.jsx
+++ Hackathon/client/src/App.jsx
@@ -1,20 +1,20 @@
 import { lazy, Suspense } from "react";
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import "./index.css";
-import { ThemeProvider, CssBaseline, CircularProgress, Box } from "@mui/material";
+import {ThemeProvider,CssBaseline,CircularProgress,Box,} from "@mui/material";
 import theme from "./components/footer/Theme.jsx";
 const LoginPage = lazy(() => import("./pages/loginpage/LoginPage.jsx"));
 const SignupPage = lazy(() => import("./pages/signup/SignupPage.jsx"));
-const EmployeeDashboard = lazy(() => import("./pages/dashboard/EmployeeDashboard.jsx"));
-const HRDashboard = lazy(() => import("./pages/dashboard/HrDashboard.jsx"));
-const ManagerDashboard = lazy(() => import("./pages/dashboard/ManagerDashboard.jsx"));
+const EmployeeDashboard = lazy(() =>import("./pages/dashboard/EmployeeDashboard.jsx"));
+const HRDashboard = lazy(() =>import("./pages/dashboard/HrDashboard.jsx"));
+const ManagerDashboard = lazy(() =>import("./pages/dashboard/ManagerDashboard.jsx"));
 const Navbar = lazy(() => import("./components/Navbar.jsx"));
 const AboutUs = lazy(() => import("./pages/navbarItems/AboutUs.jsx"));
 const Home = lazy(() => import("./components/home/Home.jsx"));
 const Contact = lazy(() => import("./pages/navbarItems/Contact.jsx"));
-const ProtectedRoute = lazy(() => import("./components/ProtectedRoute.jsx"));
+const ProtectedRoute = lazy(() =>import("./components/ProtectedRoute.jsx"));
 const Loader = () => (
-  <Box className="flex justify-center items-center min-h-screen">
+  <Box sx={{display: "flex",justifyContent: "center",alignItems: "center",minHeight: "100vh",}}>
     <CircularProgress />
   </Box>
 );
@@ -24,12 +24,13 @@ function App() {
       <CssBaseline />
       <Router>
         <Suspense fallback={<Loader />}>
+          <Navbar />
           <Routes>
-            <Route path="/" element={<Navbar />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
             <Route path="/login" element={<LoginPage />} />
             <Route path="/signup" element={<SignupPage />} />
             <Route path="/about" element={<AboutUs />} />
-            <Route path="/home" element={<Home />} />
             <Route path="/contact" element={<Contact />} />
             <Route
               path="/employeeDashboard"
